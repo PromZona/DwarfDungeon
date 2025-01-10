62e7f83b308c7ad93eb39f1ef446a5cf7ef159ea
Andrey Essaulov <andrey.essaulov@outlook.com>
2025-01-10 23:39:12 +0200

    Group Control | Easing In

--- dwarfdungeon/scripts/Unit.gd
+++ dwarfdungeon/scripts/Unit.gd
@@ -2,16 +2,46 @@ class_name Unit
 extends CharacterBody2D
 
 @export var Speed: int = 50
+@export var StartMovingTime: float = 1.0
+@export var StopMovingTime: float = 1.0
 
+enum State {Idle, Move, Attack}
+enum MoveState {Acceleration, FullSpeed, Deceleration}
+
+var state : State = State.Idle
+var move_state: MoveState = MoveState.Acceleration
 var target: Marker2D
+var start_moving_threshold = 10 
+
+var acceleration_time: float = 0.0
 
 func _init() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	var direction: Vector2 = global_position.direction_to(target.global_position)
-	move_and_collide(direction * Speed * delta)
-	pass
+	if state != State.Move:
+		return
+	if move_state == MoveState.Acceleration:
+		acceleration_time += delta
+		if acceleration_time >= StartMovingTime:
+			move_state = MoveState.FullSpeed
+	
+	var distance_to_target: float = global_position.distance_to(target.global_position)
+	if distance_to_target > start_moving_threshold:
+		var direction: Vector2 = global_position.direction_to(target.global_position)
+		var norm_coef = smoothstep(0.0, StartMovingTime, acceleration_time) 
+		var currentSpeed = ease(norm_coef, 0.3) * Speed
+		move_and_collide(direction * currentSpeed * delta)
+	return
+
+
+func start_moving() -> void:
+	if state == State.Move:
+		return
+
+	state = State.Move
+	move_state = MoveState.Acceleration
+	return
 
 func setTarget(marker: Marker2D) -> void:
 	target = marker
--- dwarfdungeon/scripts/group.gd
+++ dwarfdungeon/scripts/group.gd
@@ -4,25 +4,42 @@ extends Node2D
 @export var Speed: float = 50.0
 @export var Units: Array[Unit] = []
 
-@onready var position1: Marker2D = $Position1
-@onready var position2: Marker2D = $Position2
-@onready var position3: Marker2D = $Position3
-@onready var position4: Marker2D = $Position4
-var positions: Array[Marker2D] = []
+enum GroupState {GroupIdle, GroupMoving, GroupAttack}
+
+var Positions: Array[Marker2D] = []
+var CurrentState: GroupState = GroupState.GroupIdle
 
 func _ready() -> void:
-	positions.append($Position1)
-	positions.append($Position2)
-	positions.append($Position3)
-	positions.append($Position4)
+	Positions.append($Position1)
+	Positions.append($Position2)
+	Positions.append($Position3)
+	Positions.append($Position4)
 	
 	var i: int = 0
 	for unit: Unit in Units:
-		unit.setTarget(positions[i])
+		unit.setTarget(Positions[i])
 		i += 1
 	return
 
-func _process(delta: float) -> void:
+func notify_unit_moving():
+	for unit: Unit in Units:
+		unit.start_moving()
+	return
+
+func _input(event: InputEvent) -> void:
+	if CurrentState != GroupState.GroupMoving:
+		var start_moving: bool = false
+		start_moving = start_moving || event.is_action_pressed("move_left")
+		start_moving = start_moving || event.is_action_pressed("move_right")
+		start_moving = start_moving || event.is_action_pressed("move_up")
+		start_moving = start_moving || event.is_action_pressed("move_down")
+		if start_moving:
+			CurrentState = GroupState.GroupMoving
+			notify_unit_moving()
+		return
+	return
+
+func move(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1.0
@@ -33,9 +50,17 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1.0
 	
+	# if direction.is_zero_approx():
+
 	position += direction.normalized() * Speed * delta
 	return
 
+func attack():
+	pass
 
-func _input(event: InputEvent) -> void:
+
+func _process(delta: float) -> void:
+	if CurrentState == GroupState.GroupMoving:
+		move(delta)
 	return
+
