3e524a7b724ef418ee541d6b7c905f00cdfde4ca
Andrey Essaulov <andrey.essaulov@outlook.com>
2025-01-11 12:06:47 +0200

    Group Control | Easings Out

--- dwarfdungeon/scripts/Unit.gd
+++ dwarfdungeon/scripts/Unit.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var Speed: int = 50
 @export var StartMovingTime: float = 1.0
 @export var StopMovingTime: float = 1.0
+@export var DecelerationDistance: float = 50.0
+
+@onready var default_font = ThemeDB.fallback_font
 
 enum State {Idle, Move, Attack}
 enum MoveState {Acceleration, FullSpeed, Deceleration}
@@ -11,34 +14,79 @@ enum MoveState {Acceleration, FullSpeed, Deceleration}
 var state : State = State.Idle
 var move_state: MoveState = MoveState.Acceleration
 var target: Marker2D
-var start_moving_threshold = 10 
+var distance_to_target: float 
 
 var acceleration_time: float = 0.0
+var current_deceleration_distance = 0.0
+
+func get_speed() -> float:
+	if state != State.Move:
+		return 0.0
+	
+	var coef: float = 1.0
+	if move_state == MoveState.Acceleration:
+		coef *= ease(smoothstep(0.0, StartMovingTime, acceleration_time), 0.3)
+	elif move_state == MoveState.Deceleration:
+		coef *= ease(smoothstep(0.0, StopMovingTime, current_deceleration_distance), 0.3)
+	
+	return Speed * coef
+
+func update_move_state(delta: float) -> void:
+	if move_state == MoveState.Acceleration:
+		acceleration_time += delta
+		if acceleration_time >= StartMovingTime:
+			move_state = MoveState.FullSpeed
+		return
+	if move_state == MoveState.FullSpeed:
+		if distance_to_target < DecelerationDistance:
+			move_state = MoveState.Deceleration
+			acceleration_time = StopMovingTime
+		return
+	if move_state == MoveState.Deceleration:
+		if distance_to_target > DecelerationDistance:
+			move_state = MoveState.FullSpeed
+			return
+		current_deceleration_distance = DecelerationDistance - distance_to_target
+		if distance_to_target <= 5.0:
+			state = State.Idle
+		return
+
 
 func _init() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	distance_to_target = global_position.distance_to(target.global_position)
+	queue_redraw()
 	if state != State.Move:
 		return
-	if move_state == MoveState.Acceleration:
-		acceleration_time += delta
-		if acceleration_time >= StartMovingTime:
-			move_state = MoveState.FullSpeed
+	update_move_state(delta)
+	var direction: Vector2 = global_position.direction_to(target.global_position)
+	var currentSpeed = get_speed()
+	move_and_collide(direction * currentSpeed * delta)
 	
-	var distance_to_target: float = global_position.distance_to(target.global_position)
-	if distance_to_target > start_moving_threshold:
-		var direction: Vector2 = global_position.direction_to(target.global_position)
-		var norm_coef = smoothstep(0.0, StartMovingTime, acceleration_time) 
-		var currentSpeed = ease(norm_coef, 0.3) * Speed
-		move_and_collide(direction * currentSpeed * delta)
 	return
 
+func _draw() -> void:
+	var color: Color = Color.WHITE
+	if state == State.Idle:
+		color = Color.YELLOW
+	if state == State.Move:
+		color = Color.BLUE
+	draw_string(default_font, Vector2(-15, 25), State.keys()[state], HORIZONTAL_ALIGNMENT_LEFT, -1, 10, color)
+
+	if move_state == MoveState.Acceleration:
+		color = Color.GREEN
+	if move_state == MoveState.FullSpeed:
+		color = Color.WHITE
+	if move_state == MoveState.Deceleration:
+		color = Color.RED
+	draw_string(default_font, Vector2(-15, 40), MoveState.keys()[move_state], HORIZONTAL_ALIGNMENT_LEFT, -1, 10, color)
+	return
 
 func start_moving() -> void:
 	if state == State.Move:
 		return
-
 	state = State.Move
 	move_state = MoveState.Acceleration
 	return
--- dwarfdungeon/scripts/group.gd
+++ dwarfdungeon/scripts/group.gd
@@ -4,6 +4,9 @@ extends Node2D
 @export var Speed: float = 50.0
 @export var Units: Array[Unit] = []
 
+@onready var default_font = ThemeDB.fallback_font
+@onready var default_font_size = ThemeDB.fallback_font_size
+	
 enum GroupState {GroupIdle, GroupMoving, GroupAttack}
 
 var Positions: Array[Marker2D] = []
@@ -27,15 +30,15 @@ func notify_unit_moving():
 	return
 
 func _input(event: InputEvent) -> void:
-	if CurrentState != GroupState.GroupMoving:
-		var start_moving: bool = false
-		start_moving = start_moving || event.is_action_pressed("move_left")
-		start_moving = start_moving || event.is_action_pressed("move_right")
-		start_moving = start_moving || event.is_action_pressed("move_up")
-		start_moving = start_moving || event.is_action_pressed("move_down")
-		if start_moving:
-			CurrentState = GroupState.GroupMoving
-			notify_unit_moving()
+	var is_moving: bool = false
+	is_moving = is_moving || event.is_action_pressed("move_left")
+	is_moving = is_moving || event.is_action_pressed("move_right")
+	is_moving = is_moving || event.is_action_pressed("move_up")
+	is_moving = is_moving || event.is_action_pressed("move_down")
+
+	if CurrentState == GroupState.GroupIdle && is_moving:
+		CurrentState = GroupState.GroupMoving
+		notify_unit_moving()
 		return
 	return
 
@@ -50,7 +53,8 @@ func move(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1.0
 	
-	# if direction.is_zero_approx():
+	if direction.is_zero_approx():
+		CurrentState = GroupState.GroupIdle
 
 	position += direction.normalized() * Speed * delta
 	return
@@ -62,5 +66,18 @@ func attack():
 func _process(delta: float) -> void:
 	if CurrentState == GroupState.GroupMoving:
 		move(delta)
+	queue_redraw()
 	return
 
+func _draw() -> void:
+	# var str_pos: Vector2  = Vector2(-80, -120)
+	# var str_format_state: String = "State: %s"
+	# var str_format_move_state: String = "MoveState: %s"
+	# for unit: Unit in Units:
+	#	var text: String = str_format_state % unit.State.keys()[unit.state]
+	#	draw_string(default_font, str_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+	#	str_pos.y += 20
+	#	text = str_format_move_state % unit.MoveState.keys()[unit.move_state]
+	#	draw_string(default_font, str_pos, text, HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+	#	str_pos.y += 50
+	return
